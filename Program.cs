e5b85950516a5369c6b4a30177affe816799b422
Damian <damian.deligdisch@davinci.edu.ar>
2018-05-24 17:13:11 -0300

    Enviado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            var x = new db();
+            var a = x.autos.Where(i => i.marca == "Ford").ToList();
+
+
+
+        }
+    }
+}
---
+++ autos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class autos
+    {
+        public int Id { get; set; }
+        public String marca { get; set; }
+        public int precio {get; set;}
+    }
+}
